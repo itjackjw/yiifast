c6347fd3ba54aa0ebbcafe7f6a3d2894c39d695a
root <1092428238@qq.com>
2019-09-02 12:58:37 +0800

    修复引入文件错误

--- base/Application.php
+++ base/Application.php
@@ -15,7 +15,7 @@ class Application extends \yii\web\Application
     {
         if(!empty($addon=isset($_GET['addon'])?$_GET['addon']:$_POST['addon']) && !empty($config) && !empty($config['id'])){
            require Yii::getAlias('@addons').'/'.$addon.'/'.AddonHelper::getAppName($config['id']).'/'.'config'.'/'.'bootstrap.php';
-           require Yii::getAlias('@addons').'/'.$addon.'/common/'.'config'.'/'.'bootstrap.php',
+           require Yii::getAlias('@addons').'/'.$addon.'/common/'.'config'.'/'.'bootstrap.php';
             $config=ArrayHelper::merge(
             	$config,
             	require Yii::getAlias('@addons').'/'.$addon.'/common/'.'config'.'/'.'main.php',
